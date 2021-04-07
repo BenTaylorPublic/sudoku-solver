25216e0024c9bbce624546951c081ace3013d9ea
ben <benjaminjacktaylor@hotmail.com>
2021-04-07 12:18:21 +1000

    Algorithm working

--- src/shared/algorithms/depth-first.ts
+++ src/shared/algorithms/depth-first.ts
@@ -30,20 +30,31 @@ export class DepthFirst implements SudokuAlgorithm {
                 y: 0
             }, StepAction.FailedToAssign);
         }
-
         const firstOpenCell: CellXY = current.firstOpenCell;
-        for (let i = this.nextToAssign; i < 9; i++) {
-            if (current.isValid2(firstOpenCell, i)) {
-                //Not sure about this yet...
-                if (i < 9) {
-                    this.nextToAssign = i + 1;
-                }
-            }
+        const clone: SudokuState = SudokuState.clone(current);
+        let action: StepAction | null = null;
+
+        if (current.isValid2(firstOpenCell, this.nextToAssign)) {
+            //Should be good to insert
+            clone.cells[firstOpenCell.y][firstOpenCell.x].value = this.nextToAssign;
+            this.stack.push(clone);
+            action = StepAction.Assigned;
+        } else {
+            //Create a drawable state to show it failed to insert there
+            clone.cells[firstOpenCell.y][firstOpenCell.x].value = this.nextToAssign;
+            action = StepAction.FailedToAssign;
         }
-        return new DrawableSudokuState(current.cells, {
-            x: 0,
-            y: 0
-        }, StepAction.Assigned);
+
+        if (this.nextToAssign < 9) {
+            //Need to push the current back on the stack
+            this.stack.push(current);
+            this.nextToAssign++;
+        } else {
+            //Otherwise it is 9, so reset it to 1
+            this.nextToAssign = 1;
+        }
+        console.log("Stack size: " + this.stack.length);
+        return new DrawableSudokuState(clone.cells, firstOpenCell, action);
     }
 
     get givenUp(): boolean {
